4d8edf148262a3f2cb5d8a345c412a81103c61f8
zezo1895 <zadel1026@gmail.com>
2025-03-02 22:02:55 +0200

    'n'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, useParams, Navigate } from "react-router-dom";
+import { Route, Routes, useParams, Navigate } from "react-router-dom";  // لا تحتاج إلى BrowserRouter هنا
 import AOS from "aos";
 import NavBar from "./componant/navbar/NavBar";
 import Footer from './componant/Footer/Footer';
@@ -11,9 +11,6 @@ import Maincatgory from "./pages/maincatgory";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = useState(false);
-
-
-  
 
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
@@ -30,35 +27,30 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
-      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
-        <NavBar  handleOrderPopup={handleOrderPopup} />
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
+      <NavBar handleOrderPopup={handleOrderPopup} />
 
-        <Routes>
-          <Route path="/" element={<Home  handleOrderPopup={handleOrderPopup} />} />
-          <Route path="/admin/:secretKey" element={<AdminRoute />} />
-          <Route path="/details/:id" element={<ProductDetail />} />
-          <Route path="/category/:type" element={<Maincatgory />} />
-          <Route path="*" element={<Home  handleOrderPopup={handleOrderPopup} />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home handleOrderPopup={handleOrderPopup} />} />
+        <Route path="/admin/:secretKey" element={<AdminRoute />} />
+        <Route path="/details/:id" element={<ProductDetail />} />
+        <Route path="/category/:type" element={<Maincatgory />} />
+        <Route path="*" element={<Home handleOrderPopup={handleOrderPopup} />} />
+      </Routes>
 
-        <Footer />
-      </div>
-    </Router>
+      <Footer />
+    </div>
   );
 };
 
 // صفحة AdminRoute
 const AdminRoute = () => {
-  const { secretKey } = useParams();  // استخدم useParams لاستخراج الـ secretKey من الرابط
-  // const validSecretKey = "zezo-1832003z";  // الـ secretKey يجب أن يحتوي على القيمتين معًا
+  const { secretKey } = useParams();
   
-  // إذا كان الـ secretKey في الـ URL هو "12345-zezo"، نعرض صفحة الـ Admin، وإذا كان خاطئًا، نعيد التوجيه للصفحة الرئيسية
-  // import.meta.env.VITE_ADMIN_KEY
-  if (secretKey ==="zezo-1832003z" ) {
+  if (secretKey === "zezo-1832003z") {
     return <Admin />;
   } else {
-    return <Navigate to="/" />;  // إذا كان الـ secretKey غير صحيح، نعيد التوجيه للصفحة الرئيسية
+    return <Navigate to="/" />;
   }
 };
 
