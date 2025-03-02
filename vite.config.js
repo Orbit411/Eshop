00fe8a186db4061764001004858bfb9f89062289
zezo1895 <zadel1026@gmail.com>
2025-03-02 21:47:31 +0200

    Fix routing issue for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/eshop/", // استبدل eshop باسم المشروع إذا كان مختلفًا
+});
