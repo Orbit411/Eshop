caf466bd327c242b1066a3b37aa326dd9126cc2f
zezo1895 <zadel1026@gmail.com>
2025-03-02 21:49:11 +0200

    Fix routing issue for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // استيراد التوجيه
 import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/eshop"}> 
-      <App />
-    </BrowserRouter>
+  <React.StrictMode >
+    <App />
   </React.StrictMode>
 );
